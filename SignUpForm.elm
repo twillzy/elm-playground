38785f0341a4ab62e2222de69967ee8a6fb99889
twillzy <william.tanzy@gmail.com>
2017-05-20 01:14:02 +1000

    compiler driven debugging CDD

--- SignUpForm.elm
+++ SignUpForm.elm
@@ -1,17 +1,16 @@
 module SignUpForm exposing (..)
 
-import Html.App
 import Html exposing (..)
 import Html.Events exposing (..)
-import Html.Attributes exposing (id, type, class, value)
+import Html.Attributes exposing (id, type_, for, class, value)
 
 
 view model =
     form [ id "signup-form" ]
         [ h1 [] [ text "Sensational Signup Form" ]
         , label [ for "username-field" ] [ text "username: " ]
-        , input [ id "username-field", type "text", value model.username ] []
+        , input [ id "username-field", type_ "text", value model.username ] []
         , label [ for "password" ] [ text "password: " ]
-        , input [ id "password-field", type "password", value model.password ]
+        , input [ id "password-field", type_ "password", value model.password ] []
         , div [ class "signup-button" ] [ text "Sign Up!" ]
         ]
