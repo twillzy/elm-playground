da6f4ad1cc3532d5171215de2adec173a5f69ae2
twillzy <william.tanzy@gmail.com>
2017-05-20 01:21:41 +1000

    getErrors function to update model

--- SignUpForm.elm
+++ SignUpForm.elm
@@ -12,5 +12,20 @@ view model =
         , input [ id "username-field", type_ "text", value model.username ] []
         , label [ for "password" ] [ text "password: " ]
         , input [ id "password-field", type_ "password", value model.password ] []
+        , div [ class "validation-error" ] [ text model.errors.password ]
         , div [ class "signup-button" ] [ text "Sign Up!" ]
         ]
+
+
+getErrors model =
+    { username =
+        if model.username == "" then
+            "Plase enter a username!"
+        else
+            ""
+    , password =
+        if model.password == "" then
+            "Please enter a password!"
+        else
+            ""
+    }
