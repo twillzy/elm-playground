bd5d09e40773b40105f8c0743491b2a89a2eef72
twillzy <william.tanzy@gmail.com>
2017-05-20 14:54:54 +1000

    renamed module and rearranged code model -> update -> view

--- SignUpForm.elm
+++ SignUpForm.elm
@@ -1,20 +1,28 @@
-module SignUpForm exposing (..)
+module SignupForm exposing (..)
 
 import Html exposing (..)
 import Html.Events exposing (..)
 import Html.Attributes exposing (id, type_, for, class, value)
 
 
-view model =
-    form [ id "signup-form" ]
-        [ h1 [] [ text "Sensational Signup Form" ]
-        , label [ for "username-field" ] [ text "username: " ]
-        , input [ id "username-field", type_ "text", value model.username ] []
-        , label [ for "password" ] [ text "password: " ]
-        , input [ id "password-field", type_ "password", value model.password ] []
-        , div [ class "validation-error" ] [ text model.errors.password ]
-        , div [ class "signup-button" ] [ text "Sign Up!" ]
-        ]
+main =
+    Html.program
+        { init = ( { username = "", password = "", errors = initialErrors }, Cmd.none )
+        , view = view
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        }
+
+
+initialErrors =
+    { username = "bad username", password = "bad password" }
+
+
+update msg model =
+    if msg.msgType == "VALIDATE" then
+        ( { model | errors = getErrors model }, Cmd.none )
+    else
+        ( model, Cmd.none )
 
 
 getErrors model =
@@ -29,3 +37,16 @@ getErrors model =
         else
             ""
     }
+
+
+view model =
+    form [ id "signup-form" ]
+        [ h1 [] [ text "Sensational Signup Form" ]
+        , label [ for "username-field" ] [ text "username: " ]
+        , input [ id "username-field", type_ "text", value model.username ] []
+        , div [ class "validation-error" ] [ text model.errors.password ]
+        , label [ for "password" ] [ text "password: " ]
+        , input [ id "password-field", type_ "password", value model.password ] []
+        , div [ class "validation-error" ] [ text model.errors.password ]
+        , div [ class "signup-button" ] [ text "Sign Up!" ]
+        ]
